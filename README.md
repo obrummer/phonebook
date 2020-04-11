d075d4b292ebda4cd8111a3e1a7c76f52d3686bb
obrummer <obrummer@gmail.com>
2020-04-11 18:22:01 +0300

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://puhback123.herokuapp.com
---
+++ build/precache-manifest.97f5f4e3ad5f8f5dbc9f7aedc4da0c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1665d5925ab5c8dc18c01d02f4dbe1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd30fa188ed86a4fb687",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "5b06c8f8b2f396f15980",
+    "url": "/static/js/2.6ff66d5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ff66d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd30fa188ed86a4fb687",
+    "url": "/static/js/main.9c392ff7.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
